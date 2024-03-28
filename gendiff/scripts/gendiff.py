3eb5d2cee8aded00b7120099ed7dff86c393f557
Irina Valieva <iavalieva.ds@gmail.com>
2024-03-28 23:17:50 +0300

    step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,7 +2,11 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.parse_args()
+    parser.add_argument("first_file", type=str, help = "")
+    parser.add_argument("second_file", type=str, help = "")
+    parser.add_argument("-f", "--format", help = "set format of output")
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == '__main__':
